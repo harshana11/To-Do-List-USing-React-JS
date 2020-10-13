04c66ffef930845d436e8146256f2d2f33804e6f
unknown <harshanashanmugam@gmail.com>
2020-10-12 18:37:15 -0700

    Updates

---
+++ precache-manifest.a18b4e3d1ef4b5323ec32758af1b6428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1ddba10d2c86fdb1c46bb95cfeee34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c32f36ece61abba573e",
+    "url": "/static/css/main.ac35a3b6.chunk.css"
+  },
+  {
+    "revision": "074732deae9cd6368158",
+    "url": "/static/js/2.945c61f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.945c61f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c32f36ece61abba573e",
+    "url": "/static/js/main.69fefb35.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
